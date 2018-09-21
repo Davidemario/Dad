2551da00bd9ececaa19a792bf36e35a087a0a138
Davide <davidemario1@gmail.com>
2018-09-21 15:50:17 +0200

    3

--- New.js
+++ New.js
@@ -2,7 +2,7 @@ constconst http = require('http');
 const hostname = '127.0.0.1';
 const port = 3005;
  
-const camp = 2;
+const camp = 3;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
