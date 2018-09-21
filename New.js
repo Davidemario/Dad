a0817e818adb269439eeb853d85d364005ac4120
Davide <davidemario1@gmail.com>
2018-09-21 16:15:22 +0200

    4

--- New.js
+++ New.js
@@ -2,7 +2,11 @@ constconst http = require('http');
 const hostname = '127.0.0.1';
 const port = 3005;
  
-const camp = 3;
+<<<<<<< Updated upstream
+const camp = 8;
+=======
+const camp = 5;
+>>>>>>> Stashed changes
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
